fec6cb503bc23ee045195d7a0afc1eb91d538e9d
sshduck <d00479329@utahtech.edu>
2025-09-23 09:28:04 -0600

    updates

---
+++ client/app.js
@@ -0,0 +1,9 @@
+
+
+function load() {
+
+    fetch
+}
+
+
+load()
--- server/app.py
+++ server/app.py
@@ -1,6 +1,17 @@
-
+from flask import Flask
+from flash import request
 from dummydb import DummyDB
 
-db = DummyDB("sampledb.txt")
+app = Flask(__name__)
+
+
+@app.route("/trails", methods=["GET"])
+def get_trails():
+    db = DummyDB("sampledb.txt")
+    trails = db.readAllRecords()
+    return trails, {"Access-Control-Allow-Origin":"*"}
+
+@app.route("/trails", methods=["POST"])
+def 
 
 
